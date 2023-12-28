7ae32bd9fbd1ed8d54a2eed93622f3f8a59dc5af
Caleb <caleb.ja.owens@gmail.com>
2023-12-28 11:48:42 -0500

    no more stupid json error

--- client/src/components/Jumbotron/Jumbotron.tsx
+++ client/src/components/Jumbotron/Jumbotron.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import styles from './Jumbotron.module.css'
-import {AiFillRobot} from 'react-icons/ai'
-import { IconContext } from 'react-icons'
-=======
 import React, { useState } from "react";
 import styles from "./Jumbotron.module.css";
 import { AiFillRobot } from "react-icons/ai";
 import { IconContext } from "react-icons";
->>>>>>> efc137daf2c61d798183a0b1129b74c8142f4641
 
 const Jumbotron = () => {
   const [cookInquring, setCookInquring] = useState(false);
@@ -21,14 +14,6 @@ const Jumbotron = () => {
         </IconContext.Provider>
         <p>What can I help you with?</p>
       </div>
-<<<<<<< HEAD
-      <form>
-        <input></input>
-        <span>
-          <button></button>
-        </span>
-      </form>
-=======
       <div className={styles["options-section"]}>
         <button onClick={() => setCookInquring(true)}>
           Help me figure out what I can cook
@@ -37,7 +22,6 @@ const Jumbotron = () => {
         <button>Help me find ingredients</button>
         <button>Walk me through a recipe</button>
       </div>
->>>>>>> efc137daf2c61d798183a0b1129b74c8142f4641
     </div>
   );
 };
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,12 +5,8 @@ import Jumbotron from "../../components/jumbotron";
 import Trending from "../../components/trending";
 import Footer from "../../components/footer";
 import { useState } from "react";
-<<<<<<< HEAD:client/src/components/Home/Home.tsx
-import Menu from "../Menu";
-import React from "react";
-=======
 import Menu from "../../components/menu";
->>>>>>> efc137daf2c61d798183a0b1129b74c8142f4641:client/src/pages/Home/Home.tsx
+import React from "react";
 
 const Home = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -27,13 +23,8 @@ const Home = () => {
       <Navbar></Navbar>
       {menuOpened ? <Menu></Menu> : null}
       <Jumbotron></Jumbotron>
-<<<<<<< HEAD:client/src/components/Home/Home.tsx
-      <p>{!data ? "Loading..." : data}</p>
-
-=======
       <Trending></Trending>
       <Footer></Footer>
->>>>>>> efc137daf2c61d798183a0b1129b74c8142f4641:client/src/pages/Home/Home.tsx
     </>
   );
 };
